9dfb6bc4bf3dda549e72200c1dd5758b0fbeee03
David Svetlecic <DSvetlecic@cylance.com>
2019-04-26 09:02:37 -0500

    Add English and German translations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { FormattedMessage, FormattedHTMLMessage } from "react-intl";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">
+          <FormattedMessage
+            id="app.title"
+            defaultMessage="Welcome to {what}"
+            description="Welcome header on app main page"
+            values={{ what: "react-intl" }}
+          />
+        </h1>
+        <FormattedHTMLMessage
+          id="app.intro"
+          defaultMessage="To get started, edit <code>src/App.js</code> and save to reload."
+          description="Text on main page"
+        />
       </header>
     </div>
   );
